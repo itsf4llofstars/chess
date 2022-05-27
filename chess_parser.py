f8675cd303e870c0ffe9469d4f1e43c95d182bef
itsf4llofstars <irooted4hal@mailfence.com>
2022-05-26 19:55:01 -0500

    Inital file setup chess_parser.py
    Initial read file function

---
+++ chess_parser.py
@@ -0,0 +1,11 @@
+"""A Functions file for parsing chess *.pgn files
+Written by: itsf4llofstars
+email: irooted4hal@mailfence.com
+Date: 26 May 2022
+"""
+import re
+
+
+def pgn_file(path, filename):
+    pass
+
