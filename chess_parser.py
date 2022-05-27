fe6cbad0eb5fc76e778d68160342313aa385658e
itsf4llofstars <irooted4hal@mailfence.com>
2022-05-26 20:33:35 -0500

    Add get_games function
    Functions returns a list of strings that has only the games, lines that
    start with 1.

--- chess_parser.py
+++ chess_parser.py
@@ -41,15 +41,27 @@ def strip_new_lines(text_lines):
     return no_newlines
 
 
+def get_games(games):
+    regex_game = r'^[1]\.'
+    only_games = []
+    for game in games:
+        if re.search(regex_game, game):
+            only_games.append(game)
+    return only_games
+
+
 def main():
     """main"""
     full_path = create_full_path('/home/bumper/chess', 'chess.pgn')
-    print(full_path)
+
     pgn_games = read_file(full_path)
-    print(pgn_games)
     [print(game) for game in pgn_games]
+
     stripped_games = strip_new_lines(pgn_games)
     [print(game) for game in stripped_games]
+
+    only_the_games = get_games(stripped_games)
+    [print(game) for game in only_the_games]
 
 
 if __name__ == '__main__':
