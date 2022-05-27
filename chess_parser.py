45f1a91caa0a5c7a89069f884196ad8a7d3f0cb6
itsf4llofstars <irooted4hal@mailfence.com>
2022-05-26 20:05:28 -0500

    Add create_full_path function
    Function joins the path and filename and returns them as one
    string

--- chess_parser.py
+++ chess_parser.py
@@ -3,9 +3,27 @@ Written by: itsf4llofstars
 email: irooted4hal@mailfence.com
 Date: 26 May 2022
 """
+import os
 import re
+import sys
+
+import logger as log
 
 
-def pgn_file(path, filename):
-    pass
+def create_full_path(path, filename):
+    if not path.startswith('/') or path.endswith('/'):
+        log.error('Leading path slashes missing.')
+        print('Exiting; See chess.log')
+        sys.exit()
 
+    return os.path.join(path, filename)
+
+
+def main():
+    """main"""
+    full_path = create_full_path('/home/bumper/chess', 'chess.pgn')
+    print(full_path)
+
+
+if __name__ == '__main__':
+    main()
