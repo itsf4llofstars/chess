09e3868fb89e6fb76c0840121a5e553d3d5f272e
itsf4llofstars <irooted4hal@mailfence.com>
2022-05-26 21:25:53 -0500

    Remove general except Exception
    black

--- chess_parser.py
+++ chess_parser.py
@@ -11,38 +11,38 @@ import logger as log
 
 
 def create_full_path(path, filename):
-    if not path.startswith('/') or path.endswith('/'):
-        log.error('Leading path slashes missing.')
-        print('Exiting; See chess.log')
+    """DOC"""
+    if not path.startswith("/") or path.endswith("/"):
+        log.error("Leading path slashes missing.")
+        print("Exiting; See chess.log")
         sys.exit()
 
     return os.path.join(path, filename)
 
 
 def read_file(path):
+    """DOC"""
     try:
-        with open(path, 'r') as fo:
-            pgn_file = fo.readlines()
+        with open(path, "r") as file_object:
+            pgn_file = file_object.readlines()
     except FileNotFoundError as fnfe:
-        log.error(f'FileNotFoundError: {fnfe}')
-        print('Exiting; See chess.log')
-        sys.exit()
-    except Exception as e:
-        log.error(f'Unknown Error: {fnfe}')
-        print('Exiting; See chess.log')
+        log.error(f"FileNotFoundError: {fnfe}")
+        print("Exiting; See chess.log")
         sys.exit()
     else:
         return pgn_file
 
 
 def strip_new_lines(text_lines):
+    """DOC"""
     no_newlines = []
     [no_newlines.append(line[:-1]) for line in text_lines]
     return no_newlines
 
 
 def get_games(games):
-    regex_game = r'^[1]\.'
+    """DOC: Game must be on one singel non-wrapped line."""
+    regex_game = r"^[1]\."
     only_games = []
     for game in games:
         if re.search(regex_game, game):
@@ -52,8 +52,7 @@ def get_games(games):
 
 def main():
     """main"""
-    full_path = create_full_path('/home/bumper/chess', 'chess.pgn')
-
+    full_path = create_full_path("/home/bumper/chess", "chess.pgn")
     pgn_games = read_file(full_path)
     [print(game) for game in pgn_games]
 
@@ -64,5 +63,5 @@ def main():
     [print(game) for game in only_the_games]
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
