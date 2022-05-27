8f4649a3d5e4e170718ba14c719c1bde2fbbde72
itsf4llofstars <irooted4hal@mailfence.com>
2022-05-26 20:29:20 -0500

    Add read file, strip newlines functions
    Function reads in a text file and returns it as a list
    
    Function strips off the last char, a '\n' char, and returns the new
    lines as a list

--- chess_parser.py
+++ chess_parser.py
@@ -19,10 +19,37 @@ def create_full_path(path, filename):
     return os.path.join(path, filename)
 
 
+def read_file(path):
+    try:
+        with open(path, 'r') as fo:
+            pgn_file = fo.readlines()
+    except FileNotFoundError as fnfe:
+        log.error(f'FileNotFoundError: {fnfe}')
+        print('Exiting; See chess.log')
+        sys.exit()
+    except Exception as e:
+        log.error(f'Unknown Error: {fnfe}')
+        print('Exiting; See chess.log')
+        sys.exit()
+    else:
+        return pgn_file
+
+
+def strip_new_lines(text_lines):
+    no_newlines = []
+    [no_newlines.append(line[:-1]) for line in text_lines]
+    return no_newlines
+
+
 def main():
     """main"""
     full_path = create_full_path('/home/bumper/chess', 'chess.pgn')
     print(full_path)
+    pgn_games = read_file(full_path)
+    print(pgn_games)
+    [print(game) for game in pgn_games]
+    stripped_games = strip_new_lines(pgn_games)
+    [print(game) for game in stripped_games]
 
 
 if __name__ == '__main__':
