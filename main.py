ac9cffa27a18c2ddd32ddb9b57bdd4430bae1b78
itsf4llofstars <irooted4hal@mailfence.com>
2022-05-29 02:08:48 -0500

    Add create path call

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python3
 """main.py file for parsing chess data"""
+import chess_parser as pc
 
+path = '/home/pi/python/chess'
+filename = 'a_game.py'
 
+full_path = pc.create_full_path(path, filename)
+print(full_path)
