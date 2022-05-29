1719e474d5ae6f72f31fe2c612bdb9920d5d95b6
itsf4llofstars <irooted4hal@mailfence.com>
2022-05-29 02:25:53 -0500

    Add functions calls to write file

--- main.py
+++ main.py
@@ -2,9 +2,12 @@
 """main.py file for parsing chess data"""
 import chess_parser as pc
 
-path = '/home/pi/python/chess'
-filename = 'a_game.pgn'
+path = "/home/pi/python/chess"
+filename = "a_game.pgn"
 
 full_path = pc.create_full_path(path, filename)
 raw_games = pc.read_file(full_path)
-chess_games = pc.strip_new_lines(raw_games)
+chess_pgn = pc.strip_new_lines(raw_games)
+chess_games = pc.get_games(chess_pgn)
+print(chess_games)
+pc.write_games("new-game.txt", chess_games)
