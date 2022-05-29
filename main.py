cf1fbbe99a3aec61aed7dacb4cbb442ae9864382
itsf4llofstars <irooted4hal@mailfence.com>
2022-05-29 11:56:04 -0500

    Write main.py to use with chessfunctions.py

--- main.py
+++ main.py
@@ -1,12 +1,34 @@
 #!/usr/bin/env python3
 """main.py file for parsing chess data"""
-import chess_parser as pc
+import chessfunctions as cf
 
-path = "/home/pi/python/chess"
-filename = "a_game.pgn"
+ww = []
+bw = []
+wm = []
+bm = []
+am = []
 
-full_path = pc.create_full_path(path, filename)
-raw_games = pc.read_file(full_path)
-chess_pgn = pc.strip_new_lines(raw_games)
-chess_games = pc.get_games(chess_pgn)
-pc.write_games("new-game.txt", chess_games)
+user_path = cf.get_path()
+user_file = cf.get_filename()
+
+path_file = cf.create_full_path(user_path, user_file)  # Works
+games_file = cf.read_file(path_file)  # Works
+cleaned_games = cf.strip_new_lines(games_file)  # Works
+just_games = cf.get_games(cleaned_games)  # Works
+
+cf.write_games('bacon.pgn', just_games)  # Works
+
+# cf.wins_list(buz, ww)  # Works
+# cf.wins_list(buz, bw, False)  # Works
+
+# wws = cf.wins_str(buz)  # Works
+# bws = cf.wins_str(buz, False)  # Works
+
+# cf.mate_list(buz, wm)  # Works
+# cf.mate_list(buz, wm, False)  # Works
+
+# wms = cf.mate_str(buz)  # Works
+# bms = cf.mate_str(buz, False)  # Works
+
+# cf.all_mates_list(buz, am)  # Works
+# ams = cf.all_mates_str(buz)  # Works
